1e6713620693f87135be1aa0136dfd1be72400ae
윾비 <smbaekr56@naver.com>
2020-10-06 23:08:59 +0900

    닷트윈 + 매뉴창 만듬

---
+++ Assets/scripts/UI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public GameObject menu_set = null;
+    public GameObject Ectv_button = null;
+    public GameObject countiune_but = null;
+    public GameObject quit_but = null;
+    public void OpenMenu()
+    {
+        if(menu_set.activeSelf)
+        {
+            menu_set.SetActive(false);
+            Ectv_button.SetActive(true);
+        }
+        else
+        {
+            menu_set.SetActive(true);
+            Ectv_button.SetActive(false);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
