6bb990dfe5446894cbf170b927a89d638f89e3c8
윾비 <smbaekr56@naver.com>
2020-10-07 15:07:36 +0900

    노트 나오고 사라지는거

--- Assets/scripts/UI.cs
+++ Assets/scripts/UI.cs
@@ -14,7 +14,7 @@ public class UI : MonoBehaviour
     public GameObject Ectv_button = null;
     public GameObject countiune_but = null;
     public GameObject quit_but = null;
-    public GameObject background = null;
+    public GameObject note_set = null;
 
     private void Update()
     {
@@ -33,18 +33,42 @@ public class UI : MonoBehaviour
         {
             menu_set.SetActive(false);
             Ectv_button.SetActive(true);
-           menu_base.transform.DOMove(new Vector2(0, 7.2f), 0.5f);
+            menu_base.transform.DOMove(new Vector2(0, 7.2f), 0.5f);
+            Invoke("Off_menu", 0.5f);
         }
         else
         {
+            menu_base.SetActive(true);
             menu_set.SetActive(true);
             Ectv_button.SetActive(false);
             menu_base.transform.DOMove(new Vector2(0, 0.5f), 0.5f);
         }
     }
 
-  
+    public void OpenNote()
+    {
+        if(note_set.activeSelf)
+        {
+            note_set.transform.DOMoveX(5f,1f);
+            note_set.transform.DORotate(new Vector3(0, 0, -20), 1);
+            Invoke("Off_note", 1);
+        }
+        else
+        {
+            note_set.SetActive(true);
+            note_set.transform.DOMoveX(2, 1);
+            note_set.transform.DORotate(new Vector3(0,0,27),1);
+        }
+    }
+    private void Off_menu()
+    {
+        menu_base.SetActive(false);
+    }
 
+    private void Off_note()
+    {
+        note_set.SetActive(false);
+    }
 
 
     public void Quit()
