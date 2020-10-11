2785a6ec2249922e9f36191742c3a0e9487e009b
윾비 <smbaekr56@naver.com>
2020-10-11 21:38:28 +0900

    UI 스크립트 최적화 , 버그 수정 , 머테리얼 셰이더 변경

--- Assets/scripts/UI.cs
+++ Assets/scripts/UI.cs
@@ -8,7 +8,7 @@ public class UI : MonoBehaviour
 
     [SerializeField]
     private GameObject menu_base = null;
-    private bool is_Ectv_pressed = false;
+    private bool is_Ectv_pressed = true;
 
     public GameObject menu_set = null;
     public GameObject Ectv_button = null;
@@ -17,28 +17,35 @@ public class UI : MonoBehaviour
     public GameObject note_set = null;
     public GameObject shop_base = null;
     public GameObject gostInfo_base = null;
+    public GameObject setting_base = null;
     public GameObject gost_han_base = null;
 
-
+    private void Ectv_Active_true()
+    {
+        is_Ectv_pressed = true;
+    }
+    private void Ectv_Active_true_with_Off_Menu()
+    {
+        is_Ectv_pressed = true;
+        menu_base.SetActive(false);
+    }
     public void OpenMenu()
     {
-        if (!is_Ectv_pressed)
+        if (is_Ectv_pressed)
         {
-            is_Ectv_pressed = true;
-            Invoke("Ectv_Active", 0.51f);
+            is_Ectv_pressed = false;
 
-            if (menu_set.activeSelf)
+            if (menu_set.activeInHierarchy)
             {
                 Ectv_button.SetActive(true);
-                menu_base.transform.DOMoveX(4f, 0.5f);
                 menu_set.SetActive(false);
-               // Invoke("Off_menu", .61f);
+                menu_base.transform.DOMoveX(4f, 0.5f).OnComplete(Ectv_Active_true_with_Off_Menu);
             }
             else
             {
                 menu_base.SetActive(true);
                 menu_set.SetActive(true);
-                menu_base.transform.DOMoveX(2.5f, 0.5f);
+                menu_base.transform.DOMoveX(2.5f, 0.5f).OnComplete(Ectv_Active_true);
                 Ectv_button.SetActive(false);
             }
         }
@@ -46,49 +53,41 @@ public class UI : MonoBehaviour
 
     public void OpenNote()
     {
-        if(note_set.activeSelf)
+        if (note_set.activeInHierarchy)
         {
-            StartCoroutine(Off_note());
+            note_set.transform.DOMoveX(5f, 1f);
+            note_set.transform.DORotate(new Vector3(0, 0, -2), 1).OnComplete(All_NoteUse_Menu_Off);
             Invoke("Ectv_off", 0.001f);
         }
         else
         {
             note_set.SetActive(true);
             note_set.transform.DOMoveX(0.5f, 1f);
-            note_set.transform.DORotate(new Vector3(0,0,10),1);
+            note_set.transform.DORotate(new Vector3(0, 0, 10), 1);
             Invoke("Ectv_off", 0.001f);
         }
     }
 
-    public void Gostchack()
+    public void Gostcheck()
     {
         gost_han_base.SetActive(true);
     }
-    private void Off_menu()
+ 
+
+    private void All_NoteUse_Menu_Off()
     {
-       // menu_base.SetActive(false);
-    }
-
-    private IEnumerator Off_note()
-    {
-        note_set.transform.DOMoveX(5f, 1f);
-        note_set.transform.DORotate(new Vector3(0, 0, -2), 1);
-
-        yield return new WaitForSeconds(1f);
-
         note_set.SetActive(false);
         shop_base.SetActive(false);
         gostInfo_base.SetActive(false);
+        setting_base.SetActive(false);
     }
 
-    private void Ectv_Active()
-    {
-        is_Ectv_pressed = false;
-    }
+
+
 
     private void Ectv_off()
     {
-        if(!Ectv_button.activeSelf)
+        if (!Ectv_button.activeInHierarchy)
         {
             Ectv_button.SetActive(true);
         }
@@ -107,7 +106,7 @@ public class UI : MonoBehaviour
     // 혹시 충돌날까봐 윗줄에 안쓰고 아랫줄에 추가함요 나중에 위로 올려주시길 바람
     public void OpenGostInfo()
     {
-        if (gostInfo_base.activeSelf)
+        if (gostInfo_base.activeInHierarchy)
         {
             gostInfo_base.SetActive(false);
             OpenNote();
@@ -121,14 +120,28 @@ public class UI : MonoBehaviour
 
     public void OpenShop()
     {
-        if (shop_base.activeSelf)
+        if (shop_base.activeInHierarchy)
         {
             shop_base.SetActive(false);
             OpenNote();
         }
-        else 
+        else
         {
             shop_base.SetActive(true);
+            OpenNote();
+        }
+    }
+
+    public void OpenSetting()
+    {
+        if(setting_base.activeInHierarchy)
+        {
+            setting_base.SetActive(false);
+            OpenNote();
+        }
+        else
+        {
+            setting_base.SetActive(true);
             OpenNote();
         }
     }
