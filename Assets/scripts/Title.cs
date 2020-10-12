4f50512409373a0a0640e3e951a63b9395b9846a
윾비 <smbaekr56@naver.com>
2020-10-12 23:58:15 +0900

    스프라이트 설정 + 프레임 설정

--- Assets/scripts/Title.cs
+++ Assets/scripts/Title.cs
@@ -10,6 +10,11 @@ public class Title : MonoBehaviour
     public Image image;
     public float opacity = 0;
 
+    private void Start()
+    {
+        Application.targetFrameRate = 300;
+    }
+
     private void Awake()
     {
         Screen.orientation = ScreenOrientation.Portrait;
