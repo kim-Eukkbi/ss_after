4c7d0e36a198615706117f1445b02e58a6b97e8b
윾비 <smbaekr56@naver.com>
2020-11-16 20:25:02 +0900

    촉매 , 까마기 , 유링이 추가 촉매는 아직 더 만들어야함 일단 작동은 하는ㄷ ㅔ

--- Assets/scripts/catalyst.cs
+++ Assets/scripts/catalyst.cs
@@ -7,10 +7,38 @@ public class catalyst : MonoBehaviour
     public Gostcome gostmanager = null;
 
     [SerializeField]
-    private float catalystCount = 0f;
+    private float catalystCount = 5f;
+    [SerializeField]
+    private float catalysTimer = 120;
 
+    private float catalystOverCount = 0f;
+
+    private void Update()
+    {
+        if (catalysTimer < 0)
+        {
+            catalystOverCount--;
+            catalysTimer = 120;
+            gostmanager.comepersent = 10f;
+            return;
+        }
+        if(catalystCount < catalystOverCount)
+        {
+            catalysTimer -= Time.deltaTime;
+        }
+    }
     public void UseCatalysts()
     {
+        if(catalysTimer != 0 && catalystCount < catalystOverCount)
+        {
+            return;
+        }
+        if(catalystCount < 1)
+        {
+            Debug.Log("촉매 부족");
+            return;
+        }
+        catalystOverCount = catalystCount;
         gostmanager.comepersent = 20f;
         catalystCount--;
     }
